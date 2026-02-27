13c7d454ea32899342fc888a4a68edb12579f727
Wejua <13522599397@163.com>
2026-02-27 17:38:20 +0800

    新建WebFlux项目

---
+++ HuanJu_WebFlux/src/test/java/jieyi/lu/huanju_webflux/HuanJuWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package jieyi.lu.huanju_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuanJuWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

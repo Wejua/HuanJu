f50f55bfdbd34906ef2b34a9d71ed2a900c8da6d
Wejua <13522599397@163.com>
2026-02-28 11:10:23 +0800

    新增Flux版，添加了常用库

---
+++ HuanJuWeFlux/src/main/java/jieyi/lu/huanjuweflux/HuanJuWeFluxApplication.java
@@ -0,0 +1,13 @@
+package jieyi.lu.huanjuweflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuanJuWeFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuanJuWeFluxApplication.class, args);
+    }
+
+}
---
+++ HuanJuWeFlux/src/test/java/jieyi/lu/huanjuweflux/HuanJuWeFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package jieyi.lu.huanjuweflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuanJuWeFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
